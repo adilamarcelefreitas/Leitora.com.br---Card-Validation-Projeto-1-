32b4e56b23fbfe3937721eb398cd39449b9a9260
Ádila Freitas <adilamarcele@hotmail.com>
2023-08-10 21:31:37 -0300

    Refatoração semântica

--- src/index.js
+++ src/index.js
@@ -1,49 +1,41 @@
 import validator from './validator.js';
 
 //Inserir o número do cartão
+document.querySelector('.numero-cartao-input').oninput = () => {
 
-document.querySelector('.número-cartão-input').oninput = () => {
-
-  //document.querySelector('.número-cartão').innerText = document.querySelector('.número-cartão-input').value;
-  const inputNúmeroCartão = document.querySelector('.número-cartão-input');
-  const númeroMascarado = validator.maskify(inputNúmeroCartão.value);
-  document.querySelector('.número-cartão').innerText = númeroMascarado;
+  const inputNumeroCartao = document.querySelector('.numero-cartao-input');
+  const numeroMascarado = validator.maskify(inputNumeroCartao.value);
+  document.querySelector('.numero-cartao').innerText = numeroMascarado;
 }
 
 //Inserir o nome do usuário
-
-document.querySelector('.nome-cartão-input').oninput = () => {
-  document.querySelector('.nome-no-cartão').innerText = document.querySelector('.nome-cartão-input').value;
+document.querySelector('.nome-cartao-input').oninput = () => {
+  document.querySelector('.nome-no-cartao').innerText = document.querySelector('.nome-cartao-input').value;
 }
 
 //Selecionar o mês com o seletor e opções dadas
-
-document.querySelector('.mês-input').oninput = () => {
-  document.querySelector('.mês').innerText = document.querySelector('.mês-input').value;
+document.querySelector('.mes-input').oninput = () => {
+  document.querySelector('.mes').innerText = document.querySelector('.mes-input').value;
 }
 
 //Selecionar o ano com o seletor e opções dadas
-
 document.querySelector('.ano-input').oninput = () => {
   document.querySelector('.ano').innerText = document.querySelector('.ano-input').value;
 }
 
 //Quando o mouse clicar na opção do cvv terá uma interação de rotação do cartão
-
 document.querySelector('.cvv-input').onmouseenter = () => {
   document.querySelector('.frente').style.transform = 'perspective(1000px) rotateY(-180deg)';
-  document.querySelector('.atrás').style.transform = 'perspective(1000px) rotateY(0deg)';
+  document.querySelector('.atras').style.transform = 'perspective(1000px) rotateY(0deg)';
 }
 
 //Quando o mouse sair de cima da opção cvv o cartão faz nova rotação para parte da frente
-
 document.querySelector('.cvv-input').onmouseleave = () => {
   document.querySelector('.frente').style.transform = 'perspective(1000px) rotateY(0deg)';
-  document.querySelector('.atrás').style.transform = 'perspective(1000px) rotateY(180deg)';
+  document.querySelector('.atras').style.transform = 'perspective(1000px) rotateY(180deg)';
 }
 
 //Inserir o valor do cvv com referência de no máximo 3 digítos
-
 document.querySelector('.cvv-input').oninput = () => {
   document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
 }
@@ -52,28 +44,27 @@ document.querySelector('.cvv-input').oninput = () => {
 //uma variável chamada statusValidação para pegar a class no html com do DOM
 // fazer validar ou invalidar o cartão com a resposta e com mudança de cor
 
-function updateValidaçãoStatus(éVálido) {
-  const statusValidação = document.querySelector('.validação-status');
-  statusValidação.innerText = éVálido ? 'cartão válido' : 'cartão inválido';
-  statusValidação.style.color = éVálido ? 'green' : 'red';
+function updateValidaçaoStatus(Validaçao) {
+
+  const statusValidaçao = document.querySelector('.validaçao-status');
+
+  statusValidaçao.innerText = Validaçao ? 'cartão válido' : 'cartão inválido';
+  statusValidaçao.style.color = Validaçao ? 'green' : 'red';
 }
 
 // Evento de validação feito com o butão submeter chamado pelo seletor do DOM
-
 document.querySelector('.submeter-btn').addEventListener('click', function (event) {
   event.preventDefault();
 
   //variável para pega os números do cartão 
-
-  const númeroCartãoCrédito = document.querySelector('.número-cartão-input').value;
+  const numeroCartaoCredito = document.querySelector('.numero-cartao-input').value;
 
   // Checa se o cartão de crédito é válido, usando a função validator
-
-  const éVálido = validator.isValid(númeroCartãoCrédito);
+  const validaçao = validator.isValid(numeroCartaoCredito);
 
   // Update do status da validação do cartão
+  updateValidaçaoStatus(validaçao);
 
-  updateValidaçãoStatus(éVálido);
 });
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,68 +1,61 @@
 const validator = {
   // Função para validar o cartão usando o algoritmo Luhn
 
-  isValid: function (creditCardNumber) {
+  isValid: function (numeroCartaoCredito) {
+    //remove caracteres não numéricos etc
 
-    // variável remove caracteres não numéricos etc
-
-    const novoNúmero = creditCardNumber.replace(/\D/g, '');
-
+    const novoNumero = numeroCartaoCredito.replace(/\D/g, "");
 
     // Verifica se está vazio ou não é um número
 
-    if (novoNúmero === '' || isNaN(novoNúmero)) {
+    if (novoNumero === "" || isNaN(novoNumero)) {
       return false;
     }
 
-    //Duas váriavéis para fazer as condicionais
-
     let soma = 0;
-    let dobrarNúmero = false;
+    let dobrarNumero = false;
 
-    // Interar os dígitos na ordem inversa 
-    //usar parseInt para pegar o número da const novoNúmero e reconhever entre 0-9
-    //usar charArt para pegar dígito dentro da variável let n
+    //Usar parseInt para pegar o número da const novoNúmero e reconhever entre 0-9
+    //Usar charArt para pegar dígito dentro da variável let n
 
-    for (let n = novoNúmero.length - 1; n >= 0; n--) {
-      let dígito = parseInt(novoNúmero.charAt(n), 10);
+    for (let n = novoNumero.length - 1; n >= 0; n--) {
+      let digito = parseInt(novoNumero.charAt(n), 10);
 
-      if (dobrarNúmero) {
-        dígito *= 2;
+      if (dobrarNumero) {
+        digito *= 2;
 
         // Se dobrar o resultado em um número de dois dígitos, somar os dígitos do resultado
 
-        if (dígito > 9) {
-          dígito -= 9;
+        if (digito > 9) {
+          digito -= 9;
         }
       }
       // adicionar o valor da variável dígito na variavel soma
 
-      soma += dígito;
+      soma += digito;
 
       //inverter o valor da variável. Se o valor for verdadeira ele retorna falso, se for falso retorna verdadeiro
 
-      dobrarNúmero = !dobrarNúmero;
+      dobrarNumero = !dobrarNumero;
     }
 
     // O cartão será válido se a soma for divísivel por 10. Se for e o final for igual a 0 dá certo
     return soma % 10 === 0;
   },
 
+  // função de mascarar números
+  maskify: function (numeroCartaoCredito) {
+    //armazena e só reconhece número
+    const numerosCartao = String(numeroCartaoCredito).replace(/\D/g, "");
 
-  maskify: function (CreditCardNumber) {
-    //variavél para pegar número do cartão e utilizar a String para ter certeza q é uma string e remover caracteres não númericos 
-    const númerosCartão = String(CreditCardNumber).replace(/\D/g, '');
+    //faz a repartição dos últimos quatro dígitos
+    const ultimoQuatroDigitos = numerosCartao.slice(-4);
 
-    // variável para manter apenas os últimos quatro números do cartão
-    const últimoQuatroDígitos = númerosCartão.slice(-4);
+    //números mascarados em # mas últimos quatro dígitos
+    const numeroMascarado = "#### #### #### " + ultimoQuatroDigitos;
 
-    //Criar uma variavel para mascarar dos números com # e concatenar(juntar) com os últimos 4 dígitos
-    const númeroMascarado = '#### #### #### ' + últimoQuatroDígitos;
-
-    return númeroMascarado;
-
-  }
-
+    return numeroMascarado;
+  },
 };
 
 export default validator;
